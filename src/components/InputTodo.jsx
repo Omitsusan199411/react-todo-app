593a598429a73a4894e7281db6442ca53f495a89
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-04-28 23:36:20 +0900

    未完了TODO部品化　完了

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export const InputTodo = (props) => {
   // 親コンポーネント（App.jsx）から「props.todoText」、「props.onChange」、「props.onClick」として子コンポーネント（InputTodo.jsx）に渡される
+  // propsはオブジェクトとして渡される
+  // 下記は親から渡されたpropsを分割代入する
   const { todoText, onChange, onClick } = props;
   return (
     <>
