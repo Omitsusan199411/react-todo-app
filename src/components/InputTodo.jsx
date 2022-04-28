48c71ce25bd5ff9c30fcfb8ce9085c23b8108f70
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-04-29 01:24:08 +0900

    入力フォーム、非活性の条件を追加 未完了TODOが5件以上溜まると入力フォームと追加ボタンを非活性にする

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -4,7 +4,7 @@ export const InputTodo = (props) => {
   // 親コンポーネント（App.jsx）から「props.todoText」、「props.onChange」、「props.onClick」として子コンポーネント（InputTodo.jsx）に渡される
   // propsはオブジェクトとして渡される
   // 下記は親から渡されたpropsを分割代入する
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
   // jsxのルールに従ってcss実装
   const style = {
     backgroundColor: "#c1ffff",
@@ -18,8 +18,16 @@ export const InputTodo = (props) => {
   return (
     <>
       <div style={style} className="input-area">
-        <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
-        <button onClick={onClick}>追加</button>
+        {/* disabledにはtrueかfalseが入る */}
+        <input
+          disabled={disabled}
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChange}
+        />
+        <button disabled={disabled} onClick={onClick}>
+          追加
+        </button>
       </div>
     </>
   );
