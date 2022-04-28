aaab579c897d5eac34598c23eb7dc5b6346dd0c4
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-04-28 22:26:58 +0900

    完了ボタン実装

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,36 @@ import "./styles.css";
 
 export const App = () => {
   const [todoText, setTodoText] = useState(``);
+  // 未完了TODO State
   const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]);
+  // 完了TODO State
   const [completeTodos, setCompleteTodos] = useState(["ううう"]);
   const onChangeTodoText = (event) => setTodoText(event.target.value);
+  // 追加ボタン機能
   const onClickAdd = () => {
     if (todoText === ``) return;
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
     setTodoText(``);
   };
+  // 削除ボタン機能
   const onClickDelete = (index) => {
     const newTodos = [...incompleteTodos];
     // splice(どのインデックス番号から, 何個要素を削除)
     newTodos.splice(index, 1);
     setIncompleteTodos(newTodos);
   };
+  // 完了ボタン機能
+  const onClickComplete = (index) => {
+    // 未完了TODOの削除
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+    setIncompleteTodos(newIncompleteTodos);
+    // 完了TODOに追加
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+  };
+  // レンダリング対象
   return (
     <>
       <div className="input-area">
@@ -35,7 +50,7 @@ export const App = () => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
